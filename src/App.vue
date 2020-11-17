f626768e36489fe88c8e32558ba44e61ea5c137d
emanuellopess <lopes.emanuel@linknacional.com>
2020-11-17 16:37:14 -0300

    exemplo de component

--- src/App.vue
+++ src/App.vue
@@ -13,61 +13,8 @@
     <q-page-container>
       <div class="q-pa-lg">
         <div class="row">
-          <div id="col-start" class="col-12 col-sm-7 col-md-8 shadow-4">
-            <div class="q-px-xs">
-              <div class="q-ma-none q-pb-lg">
-                <h5>Criar uma conta</h5>
-                <br />
-                <div id="text-hform">
-                  Informe um endereço de e-mail válido e uma senha para
-                  acompanhar sua solicitação online.
-                </div>
-              </div>
-              <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-                <q-input
-                  filled
-                  v-model="email"
-                  type="email"
-                  label="Seu email"
-                  hint="Insira um email válido"
-                />
-
-                <q-input
-                  v-model="password"
-                  filled
-                  :type="isPwd ? 'password' : 'text'"
-                  hint="Criar uma senha de acesso"
-                >
-                  <template v-slot:append>
-                    <q-icon
-                      :name="isPwd ? 'visibility_off' : 'visibility'"
-                      class="cursor-pointer"
-                      @click="isPwd = !isPwd"
-                    />
-                  </template>
-                </q-input>
-
-                <q-input
-                  filled
-                  v-model="phone"
-                  label="Telefone"
-                  mask="(##) ##### - ####"
-                  fill-mask
-                  hint="Whatsapp ou Telegram"
-                />
-
-                <div>
-                  <q-btn label="Criar conta" type="submit" color="primary" />
-                  <q-btn
-                    label="Mais informações"
-                    type="reset"
-                    color="primary"
-                    flat
-                    class="q-ml-sm"
-                  />
-                </div>
-              </q-form>
-            </div>
+          <div id="col-start" class="col-12 col-sm-7 col-md-8">
+            <formulario />
           </div>
 
           <div id="col-end" class="col-12 col-sm-5 col-md-4">
@@ -306,8 +253,13 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue';
+import formulario from "./components/formulario.vue";
 
 export default {
+  components: {
+    formulario
+  },
+
   data() {
     return {
       password: "",
---
+++ src/components/formulario.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="q-px-xs">
+    <div class="q-ma-none q-pb-lg">
+      <h5>Criar uma conta</h5>
+      <br />
+      <div id="text-hform">
+        Informe um endereço de e-mail válido e uma senha para acompanhar sua
+        solicitação online.
+      </div>
+    </div>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="email"
+        type="email"
+        label="Seu email"
+        hint="Insira um email válido"
+      />
+
+      <q-input
+        v-model="password"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+        hint="Criar uma senha de acesso"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+
+      <q-input
+        filled
+        v-model="phone"
+        label="Telefone"
+        mask="(##) ##### - ####"
+        fill-mask
+        hint="Whatsapp ou Telegram"
+      />
+
+      <div>
+        <q-btn label="Criar conta" type="submit" color="primary" />
+        <q-btn
+          label="Mais informações"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  name: "formulario"
+};
+</script>
